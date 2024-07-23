8cf0b41d6502db508beb6727b734775e1644e245
marklouis123 <mlgarces321@gmail.com>
2024-07-23 09:53:17 +0800

    episode 6 part 1

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', );
 
-Route::get('/feed', function () {
-    return view('feed');
-});
 
-Route::get('/profile', function () {
-    return view('users.profile');
-});
